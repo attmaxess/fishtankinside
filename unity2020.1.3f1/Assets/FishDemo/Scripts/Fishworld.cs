30a2f98493e3b07374fcb2eb1ac3c16ed7ded52e
NTQ-ThanDo <than.do@ntq-solution.com.vn>
2020-09-16 11:13:55 +0700

    add

---
+++ unity2020.1.3f1/Assets/FishDemo/Scripts/Fishworld.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class Fishworld : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Transform fishWorldBody;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [ContextMenu("PlaceFishtank")]
+    public void PlaceFishtank()
+    {
+        Fishworld fishworld = FindObjectOfType<Fishworld>();
+        ARPlane[] arPlanes = FindObjectsOfType<ARPlane>();
+        Transform camera = FindObjectOfType<ARCameraManager>().transform;
+        ARPlane closestARPlane = null;
+        float closestDistance = float.MaxValue;
+        foreach (ARPlane arPlane in arPlanes)
+        {
+            if ((arPlane.transform.position - camera.position).magnitude < closestDistance)
+            {
+                closestDistance = (arPlane.transform.position - camera.position).magnitude;
+                closestARPlane = arPlane;
+            }
+        }
+        if (closestARPlane == null) return;
+        fishworld.transform.position = closestARPlane.centerInPlaneSpace;
+        fishWorldBody.gameObject.SetActive(true);
+    }
+}
---
+++ unity2020.1.3f1/Assets/Scripts/Welcome/DetectPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DetectPlatform : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public float waitLoadPlatform = .5f;
+
+    /// <summary>
+    /// Scenepaths
+    /// </summary>
+    public string pcScene;
+    public string androidScene;
+    public string iosScene;    
+
+    void Start()
+    {
+        Debug.Log(Application.platform);
+        StartCoroutine(LoadSceneByPlatform());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator LoadSceneByPlatform()
+    {
+        yield return new WaitForSeconds(waitLoadPlatform);
+#if UNITY_ANDROID
+        SceneManager.LoadScene(androidScene);
+#elif UNITY_IOS
+        SceneManager.LoadScene(iosScene);
+#elif UNITY_STANDALONE
+        SceneManager.LoadScene(pcScene);
+#endif
+        yield break;
+    }
+}
